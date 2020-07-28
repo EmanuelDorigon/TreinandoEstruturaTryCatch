b5a5af1cb4d13ecf9d7bddbf08124bffa0927114
Emanuel Dorigon <emanueldorigon123@gmail.com>
2020-07-28 14:24:46 -0300

    Finish program

--- TreinandoEstruturaTryCatch/Program.cs
+++ TreinandoEstruturaTryCatch/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using TreinandoEstruturaTryCatch.Entities;
+using TreinandoEstruturaTryCatch.Entities.Exceptions;
 
 namespace TreinandoEstruturaTryCatch
 {
@@ -6,7 +8,39 @@ namespace TreinandoEstruturaTryCatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime dateIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime dateOut = DateTime.Parse(Console.ReadLine());
+                Reservation reservation = new Reservation(number, dateIn, dateOut);
+                Console.WriteLine(reservation);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                dateIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                dateOut = DateTime.Parse(Console.ReadLine());
+                reservation.UpdatesDates(dateIn, dateOut);
+                Console.WriteLine(reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
         }
     }
 }
