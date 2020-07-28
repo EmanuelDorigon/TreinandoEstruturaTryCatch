b9cd0181c703906c720f2e53ae208fa255371056
Emanuel Dorigon <emanueldorigon123@gmail.com>
2020-07-28 08:57:11 -0300

    Removed Hello World

--- TreinandoEstruturaTryCatch/Program.cs
+++ TreinandoEstruturaTryCatch/Program.cs
@@ -6,7 +6,7 @@ namespace TreinandoEstruturaTryCatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
